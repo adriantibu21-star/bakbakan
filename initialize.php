e02037ac7920fab068e16b8dfe5da8f75b644f2c
Jesse Patrick Cabrera <jessepatrickcabrera@gmail.com>
2025-10-08 19:24:08 +0800

    Fix for error in logging in. Comment config file

--- initialize.php
+++ initialize.php
@@ -1,16 +1,13 @@
-<!-- Staging -->
 <?php
+// // Staging
 if(!defined('base_url')) define('base_url','http://128.199.103.91/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"eSabong2025@@new");
 if(!defined('DB_NAME')) define('DB_NAME',"monsterbet_db");
-?>
 
-
-<!-- Local -->
-<?php
+// Local
 // if(!defined('base_url')) define('base_url','http://localhost/EsProd/');
 // if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
