5510e4304686132c8a5c761ef582a508b0db7ef4
Jesse Patrick Cabrera <jessepatrickcabrera@gmail.com>
2025-11-05 22:52:43 +0800

    Update: Match config to prod to avoid conflict

--- initialize.php
+++ initialize.php
@@ -1,8 +1,8 @@
 <?php
-if(!defined('base_url')) define('base_url','http://128.199.103.91/');
+if(!defined('base_url')) define('base_url','https://www.internationalslasher.live/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"eSabong2025@@new");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"wduser01");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"petadsfesdf121212");
 if(!defined('DB_NAME')) define('DB_NAME',"monsterbet_db");
 ?>
